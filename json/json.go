48e273761f8d0860948c6095ef3a3b96a568c03c
fengjx <fengjianxin2012@gmail.com>
2023-12-26 20:39:22 +0800

    json 延迟序列化支持

--- json/json.go
+++ json/json.go
@@ -4,6 +4,7 @@ import (
 	jsoniter "github.com/json-iterator/go"
 )
 
+// ToJson 对象转 json 字符串
 func ToJson(data interface{}) (string, error) {
 	b, err := jsoniter.Marshal(data)
 	if err != nil {
@@ -12,10 +13,19 @@ func ToJson(data interface{}) (string, error) {
 	return string(b), nil
 }
 
+// ToJsonDelay 对象转 json 字符串，在调用 String() 方法时才会执行转换方法
+func ToJsonDelay(data interface{}) *DelayJsoniter {
+	return &DelayJsoniter{
+		data: data,
+	}
+}
+
+// ToBytes 对象转字节数组
 func ToBytes(data interface{}) ([]byte, error) {
 	return jsoniter.Marshal(data)
 }
 
+// FromJson json 字符串转对象
 func FromJson(jsonStr string, target interface{}) error {
 	err := jsoniter.Unmarshal([]byte(jsonStr), &target)
 	if err != nil {
@@ -24,6 +34,7 @@ func FromJson(jsonStr string, target interface{}) error {
 	return nil
 }
 
+// FromBytes 字节数组转对象
 func FromBytes(bytes []byte, target interface{}) error {
 	err := jsoniter.Unmarshal(bytes, &target)
 	if err != nil {
@@ -32,10 +43,26 @@ func FromBytes(bytes []byte, target interface{}) error {
 	return nil
 }
 
+// GetNodeFromString 通过路径，快速从json字符串解析数据
 func GetNodeFromString(jsonStr string, path interface{}) jsoniter.Any {
 	return jsoniter.Get([]byte(jsonStr), path)
 }
 
+// GetNodeFromBytes 通过路径，快速从字节数组解析数据
 func GetNodeFromBytes(byt []byte, path interface{}) jsoniter.Any {
 	return jsoniter.Get([]byte(byt), path)
+}
+
+// DelayJsoniter 通过重写 string() 方法来延迟执行json序列化
+type DelayJsoniter struct {
+	data any
+}
+
+// String 返回json字符串，如果失败则返回error信息
+func (d *DelayJsoniter) String() string {
+	jsonStr, err := ToJson(d.data)
+	if err != nil {
+		return err.Error()
+	}
+	return jsonStr
 }
--- json/json_test.go
+++ json/json_test.go
@@ -1,11 +1,21 @@
-package json
+package json_test
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/fengjx/go-halo/json"
 )
 
 func TestGetPathVal(t *testing.T) {
 	jsonStr := `{"code":1,"message":"success","result":{"totalMsgCount":0},"time":"2023-03-18 23:30:54"}`
-	assert.Equal(t, "2023-03-18 23:30:54", GetPathVal(jsonStr, "time").ToString())
+	assert.Equal(t, "2023-03-18 23:30:54", json.GetNodeFromString(jsonStr, "time").ToString())
+}
+
+func TestDelay(t *testing.T) {
+	data := map[string]any{
+		"foo": "bar",
+	}
+	t.Logf("delay json: %s", json.ToJsonDelay(data))
 }
