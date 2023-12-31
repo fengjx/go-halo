34c2f3e0c030764bfb2424159d7f19796e0d0141
fengjx <fengjianxin2012@gmail.com>
2023-12-31 19:58:15 +0800

    strings split

--- utils/string.go
+++ utils/string.go
@@ -1,7 +1,24 @@
 package utils
 
-import "github.com/samber/lo"
+import (
+	"strings"
+)
 
-func RandomString(length int) string {
-	return lo.RandomString(length, lo.LettersCharset)
+// SplitTrim 分割字符串并去掉空格
+func SplitTrim(input, sep string) []string {
+	slc := strings.Split(input, sep)
+	for i := range slc {
+		slc[i] = strings.TrimSpace(slc[i])
+	}
+	return slc
+}
+
+// SplitToSlice 分割字符串，去掉空格并遍历
+func SplitToSlice[T any](input, sep string, fn func(item string) T) []T {
+	slc := strings.Split(input, sep)
+	var res []T
+	for i := range slc {
+		res = append(res, fn(strings.TrimSpace(slc[i])))
+	}
+	return res
 }
