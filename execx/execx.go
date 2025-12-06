519835cc7b4fd0a8e26740740f1891abde1d2ddb
fengjx <fengjianxin2012@gmail.com>
2025-12-06 14:53:33 +0800

    增加控制台输出 execx

--- execx/execx.go
+++ execx/execx.go
@@ -2,8 +2,11 @@ package execx
 
 import (
 	"bytes"
+	"context"
 	"errors"
 	"fmt"
+	"io"
+	"os"
 	"os/exec"
 	"runtime"
 	"strings"
@@ -13,20 +16,17 @@ import (
 
 // from https://github.com/zeromicro/go-zero/blob/master/tools/goctl/rpc/execx/execx.go
 
-// RunFunc defines a function type of Run function
-type RunFunc func(string, string, ...*bytes.Buffer) (string, error)
-
 // Run provides the execution of shell scripts in golang,
 // which can support macOS, Windows, and Linux operating systems.
 // Other operating systems are currently not supported
-func Run(arg, dir string, in ...*bytes.Buffer) (string, error) {
+func Run(ctx context.Context, arg, dir string, in ...io.Reader) (string, error) {
 	goos := runtime.GOOS
 	var cmd *exec.Cmd
 	switch goos {
 	case osx.OsMac, osx.OsLinux:
-		cmd = exec.Command("sh", "-c", arg)
+		cmd = exec.CommandContext(ctx, "sh", "-c", arg)
 	case osx.OsWindows:
-		cmd = exec.Command("cmd.exe", "/c", arg)
+		cmd = exec.CommandContext(ctx, "cmd.exe", "/c", arg)
 	default:
 		return "", fmt.Errorf("unexpected os: %v", goos)
 	}
@@ -47,8 +47,45 @@ func Run(arg, dir string, in ...*bytes.Buffer) (string, error) {
 		}
 		return "", err
 	}
-
 	return strings.TrimSuffix(stdout.String(), osx.NL), nil
+}
+
+// RunWithConsole provides the execution of shell scripts in golang,
+// which can support macOS, Windows, and Linux operating systems.
+// Other operating systems are currently not supported
+func RunWithConsole(cmdLine, dir string) error {
+	return RunWithConsoleContext(context.Background(), cmdLine, dir)
+}
+
+// RunWithConsoleContext provides the execution of shell scripts with context support,
+// which can support macOS, Windows, and Linux operating systems.
+// Other operating systems are currently not supported
+func RunWithConsoleContext(ctx context.Context, cmdLine, dir string) error {
+	goos := runtime.GOOS
+	var cmd *exec.Cmd
+	switch goos {
+	case osx.OsMac, osx.OsLinux:
+		cmd = exec.CommandContext(ctx, "sh", "-c", cmdLine)
+	case osx.OsWindows:
+		cmd = exec.CommandContext(ctx, "cmd.exe", "/c", cmdLine)
+	default:
+		return fmt.Errorf("unexpected os: %v", goos)
+	}
+	if len(dir) > 0 {
+		cmd.Dir = dir
+	}
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	err := cmd.Run()
+	if err != nil {
+		// 如果是 context 取消导致的错误，返回 context.Canceled
+		if errors.Is(ctx.Err(), context.Canceled) {
+			return context.Canceled
+		}
+		return err
+	}
+	return nil
 }
 
 // WrapCmd wraps the command and arguments into a single string
